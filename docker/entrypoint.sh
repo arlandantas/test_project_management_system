1c39c37295d5c20410047607145036ee86d344c6
Arlan Dantas <arlanjose@gmail.com>
2025-08-18 15:58:32 -0300

    fix: Fix local franken running

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,10 +6,10 @@ if [ ! -f /app/.seeded ]; then
     php artisan db:seed && touch /app/.seeded
 fi
 
-if [[ -z "$LOCAL" ]]; then
+if [[ -n "$PRODUCTION" ]]; then
     echo "Starting production octane..."
     php artisan octane:frankenphp --port=80 --workers=4 --host=0.0.0.0 --admin-port=8081 --max-requests=100
 else
     echo "Starting local octane..."
-    php artisan octane:frankenphp --port=81 --workers=4 --host=0.0.0.0 --admin-port=8081 --max-requests=1 --watch
+    php artisan octane:frankenphp --port=80 --workers=4 --host=0.0.0.0 --admin-port=8081 --max-requests=1 --watch
 fi
