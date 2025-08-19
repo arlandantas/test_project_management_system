4a01e316f796352bc266c5d8ca959bb976caf2eb
Arlan Dantas <arlanjose@gmail.com>
2025-08-19 12:44:17 -0300

    feat: Create base migrations

---
+++ database/migrations/2025_08_19_150621_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->decimal('value', 15, 2)->unsigned();
+            $table->enum('status', ['Active', 'Inactive'])->default('Active');
+            $table->foreignId('creator_id')->constrained('users')->onDelete('cascade');
+            $table->unique(['name', 'creator_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
