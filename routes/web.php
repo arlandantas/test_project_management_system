db62e68e17c23816c06bbf708c0bd9266b92313c
Arlan Dantas <arlanjose@gmail.com>
2025-08-21 08:29:13 -0300

    fix: Create dashboard fallback endpoint

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/', function () {
     ]);
 })->middleware(['auth', 'verified'])->name('home');
 
+Route::get('/dashboard', function () {
+    return redirect('home');
+});
+
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
 require __DIR__ . '/admin.php';
