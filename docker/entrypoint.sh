7e5b108f2cc999520ef77600ce0be51b7af2f21d
Arlan Dantas <arlanjose@gmail.com>
2025-08-22 15:55:57 -0300

    feat: Seed only on non production envs

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,14 +2,14 @@
 
 php artisan migrate
 
-if [ ! -f /app/.seeded ]; then
-    php artisan db:seed && touch /app/.seeded
-fi
-
 if [[ -n "$PRODUCTION" ]]; then
     echo "Starting production octane..."
     php artisan octane:frankenphp --port=80 --workers=4 --host=0.0.0.0 --admin-port=8081 --max-requests=100
 else
+    if [ ! -f /app/.seeded ]; then
+        php artisan db:seed && touch /app/.seeded
+    fi
+
     echo "Starting local octane..."
     php artisan octane:frankenphp --port=80 --workers=4 --host=0.0.0.0 --admin-port=8081 --max-requests=1 --watch
 fi
